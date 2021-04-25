ebe852ef144f81d339828cfa17c683efbdb69983
hriddhighosh4 <hriddhi.ghosh@mykaarma.com>
2021-04-25 19:00:38 +0530

    Fixed redirect

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,20 +1,36 @@
 import React from 'react';
 import { GoogleLogout } from 'react-google-login';
+import  Home  from '../Home';
+import { useHistory } from "react-router-dom";
 
 const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+const googleStyle={
+  cta : {
+      width: '90%',
+      maxWidth: '600px',
+      marginBottom: '40px'
+  },
+}
 
 function Logout() {
+  let history = useHistory();
+
   const onSuccess = () => {
+
     console.log('Logout made successfully');
     localStorage.removeItem('name');
     localStorage.removeItem('email');
+    history.push("/");
     localStorage.setItem('isLoggedIn', false);
     window.location.reload(false);
+
+
   };
 
   return (
     <div>
-      <GoogleLogout
+
+      <GoogleLogout style={googleStyle.cta}
         clientId={clientId}
         buttonText="Logout"
         onLogoutSuccess={onSuccess}
