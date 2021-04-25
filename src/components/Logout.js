2c0e51af2db3a934947c5c5a6e323fab5c6c7607
Anjali Jain <anjali.jain@mykaarma.com>
2021-04-25 19:28:48 +0530

    revert package.json

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { GoogleLogout } from 'react-google-login';
-import  Home  from '../Home';
 import { useHistory } from "react-router-dom";
 
 const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
@@ -16,20 +15,16 @@ function Logout() {
   let history = useHistory();
 
   const onSuccess = () => {
-
     console.log('Logout made successfully');
     localStorage.removeItem('name');
     localStorage.removeItem('email');
     history.push("/");
     localStorage.setItem('isLoggedIn', false);
     window.location.reload(false);
-
-
   };
 
   return (
     <div>
-
       <GoogleLogout style={googleStyle.cta}
         clientId={clientId}
         buttonText="Logout"
